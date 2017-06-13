47db02a4bd6456f298816609d5e8255b1bac525a
megabrown <ujlujlv@naver.com>
2017-06-13 05:06:33 -0400

    Add files via upload

--- web/server/pics/function.js
+++ web/server/pics/function.js
@@ -27,22 +27,36 @@ var num10 = 1;
 var num11 = 1;
 var num12 = 1;
 
-function toggleTableAll(){
-	if((num1%2)==1 && (num2%2)==1 && (num3%2)==1 && (num4%2)==1 && (num5%2)==1 && (num6%2)==1 && (num7%2)==1 && (num8%2)==1 && (num9%2)==1 && (num10%2)==1 && (num11%2)==1 && (num12%2)==1){
-		num1 = 0;
-		num2 = 0;
-		num3 = 0;
-		num4 = 0;
-		num5 = 0;
-		num6 = 0;
-		num7 = 0;
-		num8 = 0;
-		num9 = 0;
-		num10 = 0;
-		num11 = 0;
-		num12 = 0;
-	}
-}
+jQuery(document).ready(function($) {
+	$("#cells").dblclick(function(event){
+		event.preventDefault();
+		avignon1.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos1/Untitled-1-02-1.jpg?raw=true";
+		temp1.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos1/pos1.gif?raw=true";
+		avignon2.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos2/Untitled-1-03-1.jpg?raw=true";
+		temp2.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos2/pos2.gif?raw=true";
+		avignon3.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos3/Untitled-1-04-1.jpg?raw=true";
+		temp3.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos3/pos3.gif?raw=true";
+		avignon4.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos4/Untitled-1-05-1.jpg?raw=true";
+		temp4.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos4/pos4.gif?raw=true";
+		avignon5.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos5/Untitled-1-06-1.jpg?raw=true";
+		temp5.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos5/pos5.gif?raw=true";
+		avignon6.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos6/Untitled-1-07-1.jpg?raw=true";
+		temp6.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos6/pos6.gif?raw=true";
+		avignon7.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos7/Untitled-1-08-1.jpg?raw=true";
+		temp7.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos7/pos7.gif?raw=true";
+		avignon8.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos8/Untitled-1-09-1.jpg?raw=true";
+		temp8.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos8/pos8.gif?raw=true";
+		avignon9.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos9/Untitled-1-10-1.jpg?raw=true";
+		temp9.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos9/pos9.gif?raw=true";
+		avignon10.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos10/Untitled-1-11-1.jpg?raw=true";
+		temp10.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos10/pos10.gif?raw=true";
+		avignon11.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos11/Untitled-1-12-1.jpg?raw=true";
+		temp11.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos11/pos11.gif?raw=true";
+		avignon12.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos12/Untitled-1-13-1.jpg?raw=true";
+		temp12.src="https://github.com/megabrown/android_teamPJ/blob/master/avignon/pos12/pos12.gif?raw=true";
+		num1 = num2 = num3 = num4 = num5 = num6 = num7 = num8 = num9 = num10 = num11 = num12 = 1;
+	});
+});
 
 
 function toggleTable1(){
